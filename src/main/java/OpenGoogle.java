9ae1ef4744a4b089f5e1133736a20dc6649c2d63
vihini <rvihini@gmail.com>
2026-05-05 16:49:53 +0530

    Added new automation scripts

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,15 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class OpenGoogle {
-    public static void main(String[] args) throws InterruptedException {
+
+    public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        Thread.sleep(10000);
+        WebElement searchBox = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.name("q"))
+        );
 
-        driver.quit();
+        searchBox.sendKeys("colombo");
+        searchBox.sendKeys(Keys.ENTER);
     }
 }
